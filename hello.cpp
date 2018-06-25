df0fa4cb3a4df9d6525cdc16967e232e9addb9a7
shinjitumala <funny.pig.run@gmail.com>
2018-06-25 20:27:57 +0900

    updated to include cat

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,39 @@
 #include <iostream>
 using namespace std;
 
+class Cat {
+  string name;
+  int age;
+
+public:
+  Cat(string s){
+    name = s;
+    age = 0;
+  }
+
+  void aYearHasPassed(){
+    age++;
+  }
+
+  string getName(){
+    return name;
+  }
+
+  int getAge(){
+    return age;
+  }
+};
+
 int main(){
   string s = "hello, world!";
   cout << s << endl;
+
+  Cat neko1("sam");
+  cout << "A cat is born!" << endl;
+  cout << "The cat is called \"" << neko1.getName() << "\"." << endl;
+  neko1.aYearHasPassed();
+  cout << "A year passed." << endl;
+  cout << "The cat is now " << neko1.getAge() << " years old." << endl;
+
   cout << "goodbye, world!" << endl;
 }
